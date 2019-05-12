274724ea18d8113739151550297217635407f9e9
Faure Hu <hi@faure.hu>
2019-05-12 17:55:48 -0500

    vscode

---
+++ vscode/extensions.sh
@@ -0,0 +1,13 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension
+code --install-extension alefragnani.Bookmarks
+code --install-extension atomiks.moonlight
+code --install-extension esbenp.prettier-vscode
+code --install-extension file-icons.file-icons
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension nobuhito.printcode
+code --install-extension Prisma.vscode-graphql
+code --install-extension sdras.night-owl
+code --install-extension vscodevim.vim
+code --install-extension WakaTime.vscode-wakatime
+code --install-extension yatki.vscode-surround
